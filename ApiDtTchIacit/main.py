27d3c82fbde21723e765440b3d8a73885e80efe2
Leonardo Adler da Silva <88751032+LeoAdlerr@users.noreply.github.com>
2022-09-10 01:39:43 -0300

    Add files via upload

--- ApiDtTchIacit/main.py
+++ ApiDtTchIacit/main.py
@@ -5,5 +5,11 @@ import matplotlib.pyplot as plt
 Brasilia = pd.read_csv('DBras2020.CSV', sep=';', encoding='ISO-8859-1', skiprows=8)#, , on_bad_lines = False)
 
 
-BraT = Brasilia.loc[Brasilia["Data"] == ("2020/10/10")]
-print(BraT)
+BraD = Brasilia.loc[Brasilia["Data"] == ("2020/10/10")]
+print(BraD)
+
+BraV = BraD.loc[BraD["UMIDADE REL. MIN. NA HORA ANT. (AUT) (%)"] ==('46.1')]
+print(BraV)
+
+BraDR = Brasilia[Brasilia["Data"].between("2020/01/01", "2020/01/31")]
+print(BraDR)
