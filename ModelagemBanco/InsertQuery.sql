45a68c5ad71786f3fa0fab36c27678330e7ebb27
Leonardo Adler da Silva <88751032+LeoAdlerr@users.noreply.github.com>
2022-09-12 01:35:28 -0300

    Query de insert Estação e estado_e_região
    Query para inserir os dados de Estação e estado_e_região, pois não consigo manipular os dados e inserir automaticamente

---
+++ ModelagemBanco/InsertQuery.sql
@@ -0,0 +1,11 @@
+insert into estado_e_regiao
+(eer_id, eer_regiao, eer_unidade_federativa) 
+values(1, 'CO', 'DF')
+
+INSERT INTO estacao
+(eer_id, est_codigo, est_nome_estacao, est_longitude, est_latitude, est_altitude, est_data_fundacao)
+values (1, 'A001', 'BRASILIA', -47.925756, -15.789343, 1160.96, '07/05/00')
+
+select * from estado_e_regiao
+
+select * from estacao
