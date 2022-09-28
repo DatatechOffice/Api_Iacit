39781c52e2d0c63a2b4212eb98359d4f45463b0a
Leonardo Adler da Silva <88751032+LeoAdlerr@users.noreply.github.com>
2022-09-28 16:31:04 -0300

    controller post erro 500
    controller post erro 500

--- api/src/main/java/com/iacit/api/controller/HomeController.java
+++ api/src/main/java/com/iacit/api/controller/HomeController.java
@@ -1,10 +1,11 @@
 package com.iacit.api.controller;
 
 import java.sql.Timestamp;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,36 +14,45 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.iacit.api.entity.Temperatura;
 import com.iacit.api.repository.TemperaturaRepository;
-import com.iacit.api.service.ServiceTemperatura;
+//import com.iacit.api.service.ServiceTemperatura;
 
 @Controller
 @RestController
 public class HomeController {
+
 	private Post inputs;
+	
+	private String t1;
 	
 	@Autowired(required=true)
 	private TemperaturaRepository temperatura;
-	private ServiceTemperatura serviceTemperatura;
 	
-	
-	@RequestMapping(value = { "/post" }, method = RequestMethod.POST)
-    public ServiceTemperatura post() {
+	/*
+	 * @Autowired(required=true) private ServiceTemperatura temperaturaService;
+	 */
+	@RequestMapping(value = { "/post" }, method = RequestMethod.POST) 
+    public ResponseEntity<Temperatura> post(HttpStatus accepted) {
 		
-		inputs.setDatainicio(null);
-		inputs.setDatafim(null);
-		inputs.setRegiao(null);
-		inputs.setEstacao(null);
-		inputs.setDado(null);
+		String tem_data_hora = "2020-10-10 00:00:00";
+		List<Temperatura> enviarTemperatura = temperatura.listar("A001", Timestamp.valueOf(tem_data_hora));
+		/*
+		 * inputs.setDatainicio(null); inputs.setDatafim(null); inputs.setRegiao(null);
+		 * inputs.setEstacao(null); inputs.setDado(null);
+		 */	
+		ResponseEntity<Temperatura> printnn = new ResponseEntity<Temperatura>(accepted);
 		
-        return serviceTemperatura;
+		
+		
+        return printnn ;
         }
+
 
 	@RequestMapping(value = { "/test" }, method = RequestMethod.GET)
     public ModelAndView aaaaaaa() {
         ModelAndView modelAndView = new ModelAndView();
         
     	// listaTemperatura
-    	ArrayList<Temperatura> enviarTemperatura = temperatura.listar("A001", Timestamp.valueOf("2020-01-01 00:00:00"));
+    	List<Temperatura> enviarTemperatura = temperatura.listar("A001", Timestamp.valueOf("2020-01-01 00:00:00"));
    
     	modelAndView.addObject("teste_Temperatura", enviarTemperatura);
         modelAndView.setViewName("index");
