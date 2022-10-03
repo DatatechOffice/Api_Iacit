86f3923c9299778285215c55c70735e24b021f88
Leonardo Adler da Silva <88751032+LeoAdlerr@users.noreply.github.com>
2022-10-03 11:43:36 -0300

    Delete api/src/main/java/com/iacit/api/api directory

--- api/src/main/java/com/iacit/api/api/repository/UmidadeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.iacit.api.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.iacit.api.entity.Umidade;
-
-public interface UmidadeRepository extends JpaRepository<Umidade, Integer> {
-
-}
