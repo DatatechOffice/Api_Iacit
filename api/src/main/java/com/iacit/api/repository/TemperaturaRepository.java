cd81095225f58191cbe28dbcbeb12f77d5353374
Leonardo Adler da Silva <88751032+LeoAdlerr@users.noreply.github.com>
2022-10-06 00:37:45 -0300

    TemperaturaRepository problema de sintaxe
    TemperaturaRepository com problema de sintaxe na query nativa, solucionar esse problema, pois é o último passo que falta da filtragem

--- api/src/main/java/com/iacit/api/repository/TemperaturaRepository.java
+++ api/src/main/java/com/iacit/api/repository/TemperaturaRepository.java
@@ -1,21 +1,17 @@
 package com.iacit.api.repository;
 
-import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
-
-import com.iacit.api.entity.Temperatura;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.apiIacit.DatatechApiIacit.entity.Temperatura;
+
 @Repository
 public interface TemperaturaRepository extends JpaRepository<Temperatura, Integer> {
-	@Query(value = "SELECT * FROM temperatura WHERE est_codigo = '?' AND tem_data_hora BETWEEN '?' AND '?'", nativeQuery = true)
-	public List<Temperatura> listar(
-			@Param("est_codigo") String est_codigo,
-			@Param("tem_data_hora") Timestamp tem_data_hora,
-			@Param("tem_data_hora")Timestamp tem_data_hora1
-			);
+	@Query(value = "SELECT * FROM temperatura WHERE est_codigo = 'estCodigo' AND tem_data_hora BETWEEN 'dataInicial' AND 'dataFinal'", nativeQuery = true)
+	public List<Temperatura> listar(@Param("estCodigo")String estCodigo, @Param("dataInicial")Date dataInicial, @Param("dataFinal")Date dataFinal);
 }
