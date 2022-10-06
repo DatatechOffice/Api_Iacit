56ee8991d363199c32e064e1e0455ec12d2b3107
Leonardo Adler da Silva <88751032+LeoAdlerr@users.noreply.github.com>
2022-10-06 00:27:44 -0300

    Classe chama o index

--- api/src/main/java/com/iacit/api/controller/HomeController.java
+++ api/src/main/java/com/iacit/api/controller/HomeController.java
@@ -23,43 +23,11 @@ import ch.qos.logback.core.recovery.ResilientSyslogOutputStream;
 
 @Controller
 @RestController
+@RequestMapping(value={"/"})
 public class HomeController {
-	
-	@Autowired
-	private TemperaturaRepository temperaturaRepository;
-	
-	@Autowired(required = true)
-	private ServiceTemperatura temperaturaService;
-
-	@RequestMapping(value = { "/post" }, method = RequestMethod.POST)
-	public ResponseEntity<Void> posTT(Datas data) {
-		
-		System.out.println(data.getDataInicio());
-		
-		System.out.println(data.getDataFim());
-		return temperaturaService.serviceTEntity(data.getDataInicio(), data.getDataFim()) != null ? new ResponseEntity<Void>(HttpStatus.CREATED)
-				: new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
-
-	}
-
-
-	/*
-	 * @RequestMapping(value = { "/post" }, method = RequestMethod.POST) public
-	 * ResponseEntity<Temperatura> post(HttpStatus ok) {
-	 * 
-	 * ResponseEntity<Temperatura> jsonFiltro = new ResponseEntity<Temperatura>(ok);
-	 * 
-	 * String A="2020-10-10 00:00:00";
-	 * 
-	 * String B="2020-10-11 00:00:00";
-	 * 
-	 * jsonFiltro.ok(temperaturaService.serviceTEntity(A, B));
-	 * 
-	 * return jsonFiltro ; }
-	 */
 
 	// Método para inicialização de página
-	@RequestMapping(value = { "/" }, method = RequestMethod.GET)
+	@GetMapping(value = { "/index" })
 	public ModelAndView Index() {
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.setViewName("index");
@@ -67,23 +35,9 @@ public class HomeController {
 	}
 
 	// Método para inicialização de página
-		@RequestMapping(value = { "/testeIndex" }, method = RequestMethod.GET)
+		@GetMapping(value = { "/testeIndex" })
 		public ModelAndView Index2() {
 			ModelAndView modelAndView = new ModelAndView();
 			modelAndView.setViewName("index2");
 			return modelAndView;
 		}
-
-	
-	
-	@RequestMapping(value = { "/test" }, method = RequestMethod.GET)
-	public ModelAndView aaaaaaa() {
-		ModelAndView modelAndView = new ModelAndView();
-	 
-		List<Temperatura> enviarTemperatura = temperaturaRepository.listar("A001", Timestamp.valueOf("2020-10-10 00:00:00"), Timestamp.valueOf("2020-10-10 00:00:00"));
-
-		modelAndView.addObject(enviarTemperatura);
-		
-	  return modelAndView; 
-	  }
-}
