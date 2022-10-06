65d8a66da7a926228062a0113db765ac4bb12e0b
Leonardo Adler da Silva <88751032+LeoAdlerr@users.noreply.github.com>
2022-10-06 18:18:22 -0300

    Delete api directory

--- api/src/main/java/com/iacit/api/repository/temperaturaOrvalhoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.iacit.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.iacit.api.entity.TemperaturaOrvalho;
-
-public interface TemperaturaOrvalhoRepository extends JpaRepository<TemperaturaOrvalho, Integer> {
-
-}
