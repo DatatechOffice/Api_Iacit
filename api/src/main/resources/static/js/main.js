2d17c8d8ec97a8c28bc843c7d06ec23c86c23de8
vitor <vitor02office@gmail.com>
2022-11-13 14:42:17 -0300

    Grafico utilizando o json

--- api/src/main/resources/static/js/main.js
+++ api/src/main/resources/static/js/main.js
@@ -4,7 +4,6 @@
 
 
 $.getJSON("/regiao", function(regiao) {
-			console.log(regiao)
 			var itens = [];
 			$.each(regiao, function(i){			
 			//itens.push("<tr> <td>"+ this.placa +"</td><td>"+ this.espc.modelo + "</td><td>"+ this.espc.marca + "</td><td>"+ this.espc.cor + "</td></tr>");				
@@ -151,199 +150,87 @@ document.addEventListener('click', function(eventvariavel){
 });
 
 //-----------------------------------------------------------------------------------------------------------------------------------------
-//----------------------------------------------------Criando a tabela de dados------------------------------------------------------------
+//----------------------------------------------------Botão de Filtros GRafico-------------------------------------------------------------
 //-----------------------------------------------------------------------------------------------------------------------------------------
-fetch('../data/estados.json')
+function filtra(){
+	fetch('../data/estados.json')
 	.then(response => response.json())
 	.then(data => {
 		
+	
+		const Reg = document.getElementById('Regiao').value;
 		var corpoTabela = document.querySelector('tbody');
-
-		for (let i = 1; i < data.length; i++){
-
-			var tr= document.createElement('tr');
-			var tdCodigo= document.createElement('td');
-			var tdEstacao= document.createElement('td');
-			var tdId= document.createElement('td');
-			var tdTempBulbo= document.createElement('td');
-			var tdTempMax= document.createElement('td');
-			var tdTempMin= document.createElement('td');
-			var tdDataH= document.createElement('td');
-			
-			tdCodigo.textContent = data[i].name;
-			tdEstacao.textContent =  data[i].capital;
-			tdId.textContent = data[i].abbr;
-			tdTempBulbo.textContent = data[i].lat; 
-			tdTempMax.textContent = data[i].lat;
-			tdTempMin.textContent = data[i].long;
-			tdDataH.textContent =  data[i].abbr;
-			
-			tr.appendChild(tdCodigo);
-			tr.appendChild(tdEstacao);
-			tr.appendChild(tdId);
-			tr.appendChild(tdTempBulbo);
-			tr.appendChild(tdTempMax);
-			tr.appendChild(tdTempMin);
-			tr.appendChild(tdDataH);
-			corpoTabela.appendChild(tr);
-
-		};
-	});
-
-//-----------------------------------------------------------------------------------------------------------------------------------------
-//----------------------------------------------------Criando a tabela de dados------------------------------------------------------------
-//-----------------------------------------------------------------------------------------------------------------------------------------
-(function($) {
-	"use strict";
-
-	// Spinner
-	var spinner = function() {
-		setTimeout(function() {
-			if ($("#spinner").length > 0) {
-				$("#spinner").removeClass("show");
-			}
-		}, 1);
-	};
-	spinner();
-
-	// Calender
-	$("#calender").datetimepicker({
-		inline: true,
-		format: "L",
-	});
-
-	// Calender
-	$("#calender2").datetimepicker({
-		inline: true,
-		format: "L",
-	});
-})(jQuery);
-
-function lista() {
-	google.charts.load("current", { packages: ["corechart"] });
-google.charts.setOnLoadCallback(drawChart);
- }
-
-
-
-var getData1 = function() {
-	var dataHoje = new Date();
-	dataHoje.setMinutes(dataHoje.getMinutes() - dataHoje.getTimezoneOffset());
-	var input = $("#dataInicio").val();
-
-	/*  if ($("#dataHorarioAcordado").hasClass("d-none")) return null;
-	  if (input == "" || input == " ") return "vazio";  */
-
-	var dataInput = new Date(Date.parse(input));
-	dataInput.setMinutes(dataInput.getMinutes() - dataInput.getTimezoneOffset());
-	if (dataHoje >= dataInput) {
-		return "erro";
-	}
-
-	return dataInput;
-};
-
-$(document).ready(function() {
-	$(document).on("click", "#btnEnviarDados", function() {
-		//debugger;
-
-		// Valores da data de inico e fim que foram inseridas nas entradas de data.
-		var vDataInicio = $("#dataInicio").val();
-		var vDataFim = $("#dataFim").val();
-		var vDadoDesejado = "";
-		debugger;
-
-		$.post(
-			"/filtrar/vDadoDesejado",
-			//corpo da requisição <body> == {}
-			JSON.stringify({ dataInicio: vDataInicio, dataFinal: vDataFim }),
-			function(data) {
-				if (data[0].status == 0) {
-					debugger;
-					window.location.href = "/index";
-				} else {
-					debugger;
-					sessionStorage.setItem("userNameADM", data[0].userName);
-
-					window.location.href = "/temperatura";
-				}
-			},
-			"json"
-		);
-	});
-
-
-	var OnClickLeo = function() {
-		$("#relatorio").click(function() {
-			debugger
-
-			$.getJSON({
-				url: "/testeIndex",
-				success: function(data) {
-					//KTApp.unblockPage();
-
-
-					if (data.resultado == "OK") {
-						debugger
-						window.location.reload("/testeIndex");
-					}
-					if (data.resultado == "ERROR") {
-						debugger
-						Swal.fire(
-							'Erro!',
-							'Houve um erro ao se comunicar com o servido, tente novamente mais tarde.',
-							'error'
-						);
-					}
-				}
-			})
-
-		});
-	}
-
-
-	OnClickLeo();
-
-
-})
-
-
-//Metodo script responsavel por receber e enviar os dados do front para o back atraves de um JSON
-function salvarUsuario(){
-
-	//Recebendo as informações dos filtros e armazenando em variaveis
-	var vRegiao = $("#Regiao").val();
-	var vUF = $("#UF").val();
-	var vVariavel = $("#Variavel").val();
-	var vDataInicio = $("#DataInicio").val();
-	var vDataFim = $("#DataFim").val();
-	var vEstacao = $("#Estacao").val();
-	debugger;
-	//Função ajax de atribuição do front para uma classe no java
-	$.ajax({
-		method: "POST", //Especificando qual o metodo
-		url: "/temperatura", //Definindo a url que conecta no mapeamento
-		//Passando os atribuyos da classe e quais variaveis representa eles no front
-		data: JSON.stringify({regiao : vRegiao , estado : vUF , variavel : vVariavel , dataInicio : vDataInicio , dataFim : vDataFim, estacao : vEstacao}), 
-		contentType: "application/json; charset=utf-8", //Explicitando que se trata de um conteudo JSON
-		//Alerta de sucesso ou falha no envio do JSON
-		success: function (data){
-			alert("Salvo com Sucesso!");
-			plotcharts(data);
+	
+	
+	//-----------------------------------------------------------------------------------------------------------------------------------------
+	//----------------------------------------------------Criando a tabela de dados------------------------------------------------------------
+	//-----------------------------------------------------------------------------------------------------------------------------------------
+		for (let i = 0; i < data.length; i++){
+			if(data[i].name.toLowerCase().startsWith(Reg.toLowerCase())){
+				var tr= document.createElement('tr');
+				var tdCodigo= document.createElement('td');
+				var tdEstacao= document.createElement('td');
+				var tdId= document.createElement('td');
+				var tdTempBulbo= document.createElement('td');
+				var tdTempMax= document.createElement('td');
+				var tdTempMin= document.createElement('td');
+				var tdDataH= document.createElement('td');
+				
+				tdCodigo.textContent = data[i].name;
+				tdEstacao.textContent =  data[i].capital;
+				tdId.textContent = data[i].abbr;
+				tdTempBulbo.textContent = data[i].lat; 
+				tdTempMax.textContent = data[i].lat;
+				tdTempMin.textContent = data[i].long;
+				tdDataH.textContent =  data[i].abbr;
+				
+				tr.appendChild(tdCodigo);
+				tr.appendChild(tdEstacao);
+				tr.appendChild(tdId);
+				tr.appendChild(tdTempBulbo);
+				tr.appendChild(tdTempMax);
+				tr.appendChild(tdTempMin);
+				tr.appendChild(tdDataH);
+				corpoTabela.appendChild(tr);
+	
+			};
 		}
-	}).fail(function(xhr, status, errorThrow){
-		alert("Erro ao Salvar: " + xhr.responseText);
+
+	//-----------------------------------------------------------------------------------------------------------------------------------------
+	//----------------------------------------------------Criando a Grafico--------------------------------------------------------------
+	//-----------------------------------------------------------------------------------------------------------------------------------------
+
+	//Pegando o elemnento canvas do HTML pelo id
+	plots = document.getElementById("plots");
+	
+	$.getJSON("/regiao", function(DataGrafico) {
+	console.log(DataGrafico);
+	 //Status que fica na linha y
+	var labels = DataGrafico.map(function(e) {
+		return e.regSigla;
+	 });
+	 //Status que fica na linha y
+	 var data = DataGrafico.map(function(e) {
+		return e.regId;
+	 });
+	//Criando a isntancia do objeto chart
+	new Chart(plots, {
+		type: 'line', //declara o tipo do grafico
+		data: {
+			labels: labels, //Data do alinhamento X
+			datasets: [{
+				data: data, //Data do alinhamento y
+				backgroundColor: '#5e440f',
+				borderColor: 'white',
+				fill: false, //Preenche as curvas abaixo da linha com a cor do fundo, normalmente por padrão é true
+			}]
+		},
 	});
+		
+	});
+
+
+
+});
+	
 }
-function plotcharts(data){
-			var datas=[];
-            var valor=[];
-			for(let i =0; i <data.length;i++){
-                  datas.push(data[i]['dataHora']);
-                  valor.push(parseInt(data[i]['temMax']));
-                }
-			//For Line chart
-			dataset=addData('TEMPERATURA MÍNIMA NA HORA ANT. (AUT) (°C)', valor, 'black', 'green');
-                drawchart(dataset, datas, 'line');
-                }
-                
