59cea2c8ad7abc6407b4471453d40aad77437b90
Daniloel <zoltancao@gmail.com>
2022-11-18 21:45:20 -0300

    Entidades
    Criação das entidades do usuário.

---
+++ api/src/main/java/com/iacit/api/entity/Usuario.java
@@ -0,0 +1,56 @@
+package com.iacit.api.entity;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.stereotype.Component;
+
+import it.unimi.dsi.fastutil.booleans.BooleanArrayList;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name="usuario")
+@Table(name="usuario")
+@Getter
+@Setter
+@NoArgsConstructor
+@Component
+
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name= "usu_Id")
+    private Integer usuarioId;
+
+	
+	@Column(name= "usu_Login", length = 50)
+	private String usuarioLogin;
+
+	@Column(name= "usu_Senha")
+	private Timestamp usuarioSenha;
+
+	@Column(name= "usu_Acesso")
+	private Boolean usuarioAcesso;
+	
+	
+
+	
+
+	
+
+}
