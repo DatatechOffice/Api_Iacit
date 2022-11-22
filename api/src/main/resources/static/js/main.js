8433142fcdf658f56063c68d5e6df0564d490c8b
DatatechOffice <88751032+LeoAdlerr@users.noreply.github.com>
2022-11-22 01:52:07 -0300

    erro envio de body para o back

--- api/src/main/resources/static/js/main.js
+++ api/src/main/resources/static/js/main.js
@@ -26,8 +26,6 @@ async function carregar_regiao(valRegiao){
 	}
 	
 }
-<<<<<<< Updated upstream
-
 function get_name_Regiao(name_regiao){
 	document.getElementById("Regiao").value = name_regiao;
 }
@@ -126,107 +124,6 @@ async function carregar_variavel(valvariavel){
 					if(variavelJson[i].regSigla.toLowerCase().startsWith(valvariavel.toLowerCase())){
 						html += "<li class='list-group-item list-group-item-action' onclick='get_name_variavel("+JSON.stringify(variavelJson[i].regSigla)
 						+")'>" + variavelJson[i].regSigla + "</li>";
-=======
-
-function get_name_Regiao(name_regiao){
-	document.getElementById("Regiao").value = name_regiao;
-}
-
-const fecharReg = document.getElementById('Regiao');
-
-document.addEventListener('click', function(eventReg){
-	const validar_regiao = fecharReg.contains(eventReg.target);
-	if(!validar_regiao){
-		document.getElementById('pesquisa_regiao').innerHTML = '';
-	}
-});
-//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-//---------------------------------------------------------------------------------AutoComplete-----------------------------------------------------------------------------
-//---------------------------------------------------------------------------------------UF---------------------------------------------------------------------------------
-
-async function carregar_UF(valUF){
-	if(valUF.length >= 1){
-		
-		$.getJSON("/estado", function(UFJson) {
-			console.log(UFJson);
-	
-			var html = "<ul class='list-group' position-fixed>";
-			for(let i = 0; i < UFJson.length; i++){
-				if(UFJson[i].etdUnidadeFederativa.toLowerCase().startsWith(valUF.toLowerCase())){
-					html += "<li class='list-group-item list-group-item-action' onclick='get_name_UF("+JSON.stringify(UFJson[i].etdUnidadeFederativa)+
-					")'>" + UFJson[i].etdUnidadeFederativa + "</li>";
-				}
-			}
-			html += "</ul>";
-			document.getElementById('pesquisa_UF').innerHTML = html;
-			});
-
-	}else{
-		document.getElementById('pesquisa_UF').innerHTML = '';
-	}
-}
-
-function get_name_UF(name_UF){
-	document.getElementById("UF").value = name_UF;
-}
-
-const fecharUF = document.getElementById('UF');
-
-document.addEventListener('click', function(eventUF){
-	const validar_UF = fecharUF.contains(eventUF.target);
-	if(!validar_UF){
-		document.getElementById('pesquisa_UF').innerHTML = '';
-	}
-});
-//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-//---------------------------------------------------------------------------------AutoComplete-----------------------------------------------------------------------------
-//----------------------------------------------------------------------------------Estacação--------------------------------------------------------------------------------
-async function carregar_estacao(valestacao){
-	if(valestacao.length >= 1){
-
-		$.getJSON("/estacao", function(estacaoJson) {
-
-			var html = "<ul class='list-group' position-fixed>";
-			for(let i = 0; i < estacaoJson.length; i++){
-				if(estacaoJson[i].estNomeEstacao.toLowerCase().startsWith(valestacao.toLowerCase())){
-					html += "<li class='list-group-item list-group-item-action' onclick='get_name_estacao("+JSON.stringify(estacaoJson[i].estNomeEstacao)+")'>" 
-					+ estacaoJson[i].estNomeEstacao + "</li>";
-				}
-			}
-			html += "</ul>";
-			document.getElementById('pesquisa_estacao').innerHTML = html;
-		});
-	}else{
-		document.getElementById('pesquisa_estacao').innerHTML = '';
-	}
-}
-function get_name_estacao(name_estacao){
-	document.getElementById("Estacao").value = name_estacao;
-}
-
-const fecharestacao = document.getElementById('Estacao');
-
-document.addEventListener('click', function(eventestacao){
-	const validar_estacao = fecharestacao.contains(eventestacao.target);
-	if(!validar_estacao){
-		document.getElementById('pesquisa_estacao').innerHTML = '';
-	}
-});
-//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-//---------------------------------------------------------------------------------AutoComplete-----------------------------------------------------------------------------
-//------------------------------------------------------------------------------------Variavel-------------------------------------------------------------------------------
-async function carregar_variavel(valvariavel){
-	if(valvariavel.length >= 1){
-		
-		
-		$.getJSON("/variavel", function(variavelJson) {
-
-				var html = "<ul class='list-group' position-fixed>";
-				for(let i = 0; i < variavelJson.length; i++){
-					if(variavelJson[i].toLowerCase().startsWith(valvariavel.toLowerCase())){
-						html += "<li class='list-group-item list-group-item-action' onclick='get_name_variavel("+JSON.stringify(variavelJson[i])
-						+")'>" + variavelJson[i] + "</li>";
->>>>>>> Stashed changes
 					}
 				}
 				html += "</ul>";
@@ -254,11 +151,38 @@ document.addEventListener('click', function(eventvariavel){
 //-----------------------------------------------------------------------------------------------------------------------------------------
 //----------------------------------------------------Botão de Filtros GRafico-------------------------------------------------------------
 //-----------------------------------------------------------------------------------------------------------------------------------------
-function filtra(){		
+function filtra(){
+	
+	//Recebendo as informações dos filtros e armazenando em variaveis
+	var vRegiao = $("#Regiao").val();
+	var vUF = $("#UF").val();
+	var vVariavel = $("#Variavel").val();
+	var vDataInicio = $("#DataInicio").val();
+	var vDataFim = $("#DataFim").val();
+	var vEstacao = $("#Estacao").val();
+	debugger;
+	//Função ajax de atribuição do front para uma classe no java
+	$.ajax({
+		method: "POST", //Especificando qual o metodo
+		url: "/"+vVariavel, //Definindo a url que conecta no mapeamento
+		//Passando os atribuyos da classe e quais variaveis representa eles no front
+		dataJson: JSON.stringify({regiao : vRegiao , estado : vUF , variavel : vVariavel , dataInicio : vDataInicio , dataFim : vDataFim, estacao : vEstacao}), 
+		contentType: "application/json; charset=utf-8", //Explicitando que se trata de um conteudo JSON
+		//Alerta de sucesso ou falha no envio do JSON
+		success: function (dataJson){
+			alert("Salvo com Sucesso!");
+			
+			plotcharts(dataJson);
+			
+
+		}
+	}).fail(function(xhr, status, errorThrow){
+		alert("Erro ao Salvar: " + xhr.responseText);
+	});
+			
 	//-----------------------------------------------------------------------------------------------------------------------------------------
 	//----------------------------------------------------Criando a tabela de dados------------------------------------------------------------
 	//-----------------------------------------------------------------------------------------------------------------------------------------
-<<<<<<< Updated upstream
 	const Reg = document.getElementById('Regiao').value;
 	var corpoTabela = document.querySelector('tbody');
 
@@ -296,38 +220,7 @@ function filtra(){
 		}
 		
 		});
-=======
-	//Metodo script responsavel por receber e enviar os dados do front para o back atraves de um JSON
-	//Recebendo as informações dos filtros e armazenando em variaveis
-	var vRegiao = $("#Regiao").val();
-	var vUF = $("#UF").val();
-	var vVariavel = $("#Variavel").val();
-	var vDataInicio = $("#DataInicio").val();
-	var vDataFim = $("#DataFim").val();
-	var vEstacao = $("#Estacao").val();
-	//Função ajax de atribuição do front para uma classe no java
-	$.ajax({
-		method: "POST", //Especificando qual o metodo
-		url: "/"+vVariavel, //Definindo a url que conecta no mapeamento
-		//Passando os atribuyos da classe e quais variaveis representa eles no front
-		dataJson: JSON.stringify({regiao : vRegiao , estado : vUF , variavel : vVariavel , dataInicio : vDataInicio , dataFim : vDataFim, estacao : vEstacao}), 
-		contentType: "application/json; charset=utf-8", //Explicitando que se trata de um conteudo JSON
-		//Alerta de sucesso ou falha no envio do JSON
-		success: function (dataJson){
-			alert("Salvo com Sucesso!");
-			
-			plotcharts(dataJson);
 
-		}
-	}).fail(function(xhr, status, errorThrow){
-		
-		alert("Erro ao Salvar: " + xhr.responseText);
-		console.log(dataJson);
-	});
-
-
-
->>>>>>> Stashed changes
 
 	//-----------------------------------------------------------------------------------------------------------------------------------------
 	//----------------------------------------------------Criando a Grafico--------------------------------------------------------------
@@ -335,67 +228,12 @@ function filtra(){
 
 	//Pegando o elemnento canvas do HTML pelo id
 	
-<<<<<<< Updated upstream
-	$.getJSON("/regiao", function(DataGrafico) {
-	console.log(DataGrafico);
-=======
-	
->>>>>>> Stashed changes
-
-	 //Status que fica na linha y
-	//var labels = DataGrafico.map(function(e) {
-		//return e.regSigla;
-	// });
-
-	 //Status que fica na linha y
-	 //var data = DataGrafico.map(function(e) {
-	//	return e.regId;
-	// });
-
-<<<<<<< Updated upstream
-	    // setup 
-		const data = {
-			labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-			datasets: [{
-			  label: 'Maxima',
-			  data: [18, 12, 6, 9, 12, 3, 9],
-			  backgroundColor: 'white',
-			  borderColor: 'rgba(0,0,255)',
-			  tension: 0.5,
-			},{
-				label: 'Minima',
-				data: [10, 5, 3, 7, 4, 3, 1],
-				backgroundColor: 'White',
-				borderColor: 'rgba(255,0,0)',
-				tension: 0.5,
-			  }]
-		  };
-	  
-		  // config 
-		  const config = {
-			type: 'line',
-			data,
-			options: {
-			  scales: {
-				y: {
-				  beginAtZero: true
-				}
-			  }
-			}
-		  };
-	  
-		  // render init block
-		  const myChart = new Chart(
-			document.getElementById('plots'),
-			config
-		  );		
-	});	
-}
-=======
-	    // setup 		
 
 
-			}
+	}
+
+
+
 function plotcharts(dataJson){
 			var datas=[];
             var valor=[];
@@ -439,4 +277,4 @@ function plotcharts(dataJson){
 					config
 				  );		
                 }
->>>>>>> Stashed changes
+            
