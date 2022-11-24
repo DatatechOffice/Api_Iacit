fcb8e20d26c2aab5d76a72c2b624c3de098a0c79
Daniloel <zoltancao@gmail.com>
2022-11-24 00:49:24 -0300

    Update UsuarioRepository.java

--- api/src/main/java/com/iacit/api/repository/UsuarioRepository.java
+++ api/src/main/java/com/iacit/api/repository/UsuarioRepository.java
@@ -5,18 +5,9 @@ import org.springframework.data.jpa.repository.Query;
 
 import com.iacit.api.entity.Usuario;
 
-public interface UsuarioRepository  extends JpaRepository <Usuario,Integer> {
-	
-	@Query(
-			value = "SELECT * FROM usuario WHERE usu_Login = ?1 and usu_Senha = ?2 ",
-			nativeQuery = true
-		)
-	
-		public Usuario findByest_Nome_Senha_Usuario(String nome, String senha);
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+	@Query(value = "SELECT * FROM usuario WHERE usu_Login = ?1 and usu_Senha = ?2 and usu_Acesso=?3", nativeQuery = true)
+
+	public Usuario findByest_Nome_Senha_Usuario(String nome, String senha,Boolean acesso);
 }
-
-
-
-
-	
-
