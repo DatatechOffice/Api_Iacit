5c4d4a51579db6357b046676c058c35331516dbd
Augusto Torres Santos <augustotsantos@outlook.com>
2022-11-24 19:26:50 -0300

    removido php de teste

--- api/src/main/resources/templates/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $bdHost = 'Localhost';
-    $bdUsername = 'postgres';
-    $bdPassword = '123';
-    $bdName = 'Api_Iacit';
-
-    $conexao = new postgres($bdHost, $bdUsername, $bdPassword, $bdName);
-?>
